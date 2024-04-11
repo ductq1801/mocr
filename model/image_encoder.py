74781fce643349cf703a6c969a1ce08c6df4bd75
ductq1801 <ductq1801@gmail.com>
2024-04-11 18:14:02 +0700

    new

--- model/image_encoder.py
+++ model/image_encoder.py
@@ -58,3 +58,41 @@ class AttentionBlock(nn.Module):
         else:
             output = F.adaptive_avg_pool2d(f, (1,1)).view(N,C) # global average pooling
         return a, output
+
+class Image_Encoder_v2(nn.Module):
+    def __init__(self,hidden_dim,pretrained=True,drop_out=0.2,normalize_attn=True):
+        super(Image_Encoder,self).__init__()
+        model = timm.create_model('tf_efficientnet_b5.ns_jft_in1k',pretrained=pretrained)
+        self.trans = create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
+        self.features = nn.Sequential(*list(model.children())[:-4])
+        in_features = self.features[-1][-1][-1].conv_pwl.out_channels
+        self.dropout = nn.Dropout(drop_out)
+        self.conv_1x1 = nn.Conv2d(in_features, hidden_dim, 1)
+        self.pool = nn.AvgPool2d(2, stride=1)
+        self.attn1 = AttentionBlock(hidden_dim, hidden_dim, hidden_dim, 1, normalize_attn=normalize_attn)
+    def forward(self,x):
+        """
+        Shape:
+            - x: (B, C, W, H)
+            - output: (H_dim,B, C)
+            
+        """
+        x1 = self.features(x)
+        x1 = self.dropout(x1)
+        x2 = self.conv_1x1(x1)
+        pool1 = F.max_pool2d(x1, 2, 2)
+        pool2 = F.max_pool2d(x2, 2, 2)
+        a,g = self.attn1(pool1,pool2)
+        pool2 = self.pool(pool2)
+        pool2 =  pool2.transpose(-1, -2)
+        pool2 = pool2.flatten(2)
+        pool2 = pool2.permute(0, -1, 1)
+        g = g.unsqueeze(1)
+        x = torch.cat((pool2,g),dim=1)
+
+
+        # x =  x.transpose(-1, -2)
+        # x = x.flatten(2)
+        #x = x.permute(-1, 0, 1)
+        x = x.permute(1, 0, -1)
+        return x
