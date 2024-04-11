b3de1fdb8906ede2f4e415dd91d7f36936651e9a
ductq1801 <ductq1801@gmail.com>
2024-04-11 20:31:13 +0700

    update

--- trainer.py
+++ trainer.py
@@ -53,7 +53,7 @@ class Trainer():
             self.load_weights(weight_file)
 
         
-        self.optimizer = AdamW(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09)
+        self.optimizer = AdamW(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09,lr=1e-3)
         self.scheduler = OneCycleLR(self.optimizer, total_steps=self.num_iters, **config['optimizer'])
 #        self.optimizer = ScheduledOptim(
 #            Adam(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09),
